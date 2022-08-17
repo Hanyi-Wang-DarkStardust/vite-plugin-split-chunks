24594f280661d08a164eaa0187d8817a5111a6dd
Hanyi Wang <53130378+Hanyi-Wang-DarkStardust@users.noreply.github.com>
2022-08-17 22:56:37 +0800

    ci: add eslint and prettier for project

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  // 2. 加入 prettier 的 eslint 插件
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    // 3. 注意要加上这一句，开启 prettier 自动修复的功能
+    'prettier/prettier': 'error',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
